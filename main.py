1a455122e5cb02bfd9a21e5c3d660d31f0aff525
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-08-30 07:49:08 -0700

    Detect Ball and save the video

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from utils import (read_video, 
                    save_video)
 
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 
 def main():
     # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    # Detect Players
+    # Detect Players and Ball
     player_tracker = PlayerTracker(model_path='yolov8x')
+    ball_tracker = BallTracker(model_path='models/yolo5.pt')
+
     player_detections = player_tracker.detect_frames(video_frames,
-                                                     read_from_stub=False,
+                                                     read_from_stub=True,
                                                      stub_path="tracker_stubs/player_detections.pkl"
+                                                     )
+    ball_detections = ball_tracker.detect_frames(video_frames,
+                                                     read_from_stub=False,
+                                                     stub_path="tracker_stubs/ball_detections.pkl"
                                                      )
 
     # Draw output
 
     ## Draw Player Bounding Boxes
     output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames= ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output_video.avi")
