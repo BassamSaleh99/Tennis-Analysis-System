77feca465ac9a7a9862d3d68c5401e941444bc8e
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-09-12 01:04:07 +0300

    set_canvas_background_box_position, set_mini_court_position

---
+++ mini_court/mini_court.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import sys
+sys.path.append('../')
+import constants
+
+class MiniCourt():
+    def __init__(self,frame):
+        self.drawing_rectangle_width = 250
+        self.drawing_rectangle_height = 500
+        self.buffer = 50
+        self.padding_court=20
+
+        self.set_canvas_background_box_position(frame)
+        self.set_mini_court_position()
+
+    def set_mini_court_position(self):
+        self.court_start_x = self.start_x + self.padding_court
+        self.court_start_y = self.start_y + self.padding_court
+        self.court_end_x = self.end_x - self.padding_court
+        self.court_end_y = self.end_y - self.padding_court
+        self.court_drawing_width = self.court_end_x - self.court_start_x
+
+
+    def set_canvas_background_box_position(self,frame):
+        frame= frame.copy()
+
+        self.end_x = frame.shape[1] - self.buffer
+        self.end_y = self.buffer + self.drawing_rectangle_height
+        self.start_x = self.end_x - self.drawing_rectangle_width
+        self.start_y = self.end_y - self.drawing_rectangle_height
