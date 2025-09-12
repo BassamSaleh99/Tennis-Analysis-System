a09feb5e257d7694a9f6b44a843fa040b0ad66e2
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-09-12 14:08:22 +0300

    draw lines and draw net

--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -107,6 +107,18 @@ class MiniCourt():
             x = int(self.drawing_key_points[i])
             y = int(self.drawing_key_points[i+1])
             cv2.circle(frame, (x,y),5, (0,0,255),-1)
+
+        # draw Lines
+        for line in self.lines:
+            start_point = (int(self.drawing_key_points[line[0]*2]), int(self.drawing_key_points[line[0]*2+1]))
+            end_point = (int(self.drawing_key_points[line[1]*2]), int(self.drawing_key_points[line[1]*2+1]))
+            cv2.line(frame, start_point, end_point, (0, 0, 0), 2)
+
+        # Draw net
+        net_start_point = (self.drawing_key_points[0], int((self.drawing_key_points[1] + self.drawing_key_points[5])/2))
+        net_end_point = (self.drawing_key_points[2], int((self.drawing_key_points[1] + self.drawing_key_points[5])/2))
+        cv2.line(frame, net_start_point, net_end_point, (255, 0, 0), 2)
+
         return frame
 
     def draw_background_rectangle(self,frame):
